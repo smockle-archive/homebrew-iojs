7ebfaf6b23389d30c4f1b1650131f131b8bfa92f
Clay Miller <clay@smockle.com>
2015-01-11 21:11:49 -0800

    Initial commit.

---
+++ Formula/iojs.rb
@@ -0,0 +1,121 @@
+# Note that x.even are stable releases, x.odd are devel releases
+class Iojs < Formula
+  homepage "https://iojs.org/"
+  url "https://iojs.org/download/nightly/v1.0.0-nightly201501111b8f37bb4d/iojs-v1.0.0-nightly201501111b8f37bb4d.tar.gz"
+  sha256 "c69edc1cdd13db6c98dc9c4af01d672bae69e1eca43f2d3bfa007c142e9255b3"
+  revision 1
+
+  head do
+    url "https://github.com/iojs/io.js.git", :branch => "v1.x"
+
+    depends_on "pkg-config" => :build
+    depends_on "icu4c"
+  end
+
+  deprecated_option "enable-debug" => "with-debug"
+
+  option "with-debug", "Build with debugger hooks"
+  option "without-npm", "npm will not be installed"
+  option "without-completion", "npm bash completion will not be installed"
+
+  depends_on :python => :build
+
+  fails_with :llvm do
+    build 2326
+  end
+
+  resource "npm" do
+    url "https://registry.npmjs.org/npm/-/npm-2.1.17.tgz"
+    sha1 "80fa7873188659037ec0ed8ebc95c2b2723c8ac4"
+  end
+
+  def install
+    args = %W{--prefix=#{prefix} --without-npm}
+    args << "--debug" if build.with? "debug"
+
+    # This should eventually be able to use the system icu4c, but right now
+    # it expects to find this dependency using pkgconfig.
+    if build.head?
+      ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["icu4c"].opt_prefix}/lib/pkgconfig"
+      args << "--with-intl=system-icu"
+    end
+
+    system "./configure", *args
+    system "make", "install"
+
+    resource("npm").stage libexec/"npm" if build.with? "npm"
+  end
+
+  def post_install
+    return if build.without? "npm"
+
+    (libexec/"npm").cd { system "make", "uninstall" }
+    Pathname.glob(HOMEBREW_PREFIX/"share/man/*") do |man|
+      next unless man.directory?
+      man.children.each do |file|
+        next unless file.symlink?
+        file.unlink if file.readlink.to_s.include? "/node_modules/npm/man/"
+      end
+    end
+
+    node_modules = HOMEBREW_PREFIX/"lib/node_modules"
+    node_modules.mkpath
+    cp_r libexec/"npm", node_modules
+
+    npm_root = node_modules/"npm"
+    npmrc = npm_root/"npmrc"
+    npmrc.atomic_write("prefix = #{HOMEBREW_PREFIX}\n")
+
+    # set log level temporarily for npm's `make install`
+    ENV["NPM_CONFIG_LOGLEVEL"] = "verbose"
+
+    # make sure npm can find iojs
+    ENV["PATH"] = "#{opt_bin}:#{ENV["PATH"]}"
+
+    ENV["NPM_CONFIG_USERCONFIG"] = npmrc
+    npm_root.cd { system "make", "install" }
+
+    if build.with? "completion"
+      bash_completion.install_symlink \
+        npm_root/"lib/utils/completion.sh" => "npm"
+    end
+  end
+
+  def caveats
+    s = ""
+
+    if build.with? "npm"
+      s += <<-EOS.undent
+        If you update npm itself, do NOT use the npm update command.
+        The upstream-recommended way to update npm is:
+          npm install -g npm@latest
+      EOS
+    else
+      s += <<-EOS.undent
+        Homebrew has NOT installed npm. If you later install it, you should supplement
+        your NODE_PATH with the npm module folder:
+          #{HOMEBREW_PREFIX}/lib/node_modules
+      EOS
+    end
+
+    s
+  end
+
+  test do
+    path = testpath/"test.js"
+    path.write "console.log('hello');"
+
+    output = `#{bin}/iojs #{path}`.strip
+    assert_equal "hello", output
+    assert_equal 0, $?.exitstatus
+
+    if build.with? "npm"
+      # make sure npm can find iojs
+      ENV.prepend_path "PATH", opt_bin
+      assert_equal which("iojs"), opt_bin/"iojs"
+      assert (HOMEBREW_PREFIX/"bin/npm").exist?, "npm must exist"
+      assert (HOMEBREW_PREFIX/"bin/npm").executable?, "npm must be executable"
+      system "#{HOMEBREW_PREFIX}/bin/npm", "--verbose", "install", "npm@latest"
+    end
+  end
+end
