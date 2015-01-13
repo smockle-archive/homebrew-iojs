8a9ca4a36f92cbfa09e19436b13183efed7d4013
Clay Miller <clay@smockle.com>
2015-01-13 11:31:04 -0800

    Update SHA256 hash. Fixes #1.

--- Formula/iojs.rb
+++ Formula/iojs.rb
@@ -1,7 +1,7 @@
 class Iojs < Formula
   homepage "https://iojs.org/"
   url "https://iojs.org/download/nightly/v1.0.0-nightly201501135ea716d895/iojs-v1.0.0-nightly201501135ea716d895.tar.gz"
-  sha256 "f6ac6384910d8aa635f751d880d594c434fb40247a188f3e3fdc6694586f705b"
+  sha256 "c412a95bc44197f75293962040cccfbb9a34bb5f47aa8195a413bc163d98108a"
   revision 1
 
   head do
